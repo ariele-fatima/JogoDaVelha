7af835e780f931276bca955f0c9649c90d9f6228
ariele-fatima <ariele_afd@yahoo.com.br>
2021-08-29 18:44:50 -0300

    verificando vencedor

--- scripts/interface.js
+++ scripts/interface.js
@@ -1,29 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
     let squares = document.querySelectorAll(".square");
 
-    squares.forEach((square)=>{
+    squares.forEach((square) => {
         square.addEventListener('click', handleClick);
     });
 });
 
-function handleClick(event){
+function handleClick(event) {
 
     let square = event.target;
     let position = square.id;
 
-    handleMove(position);
-    updateSquares();
+    if (handleMove(position)) {
+        setTimeout(() => {
+            alert("O Jogo Acabou - O Vencedor foi " + playerTime);
+
+        }, 10)
+    };
+
+    updateSquare(position);
 }
 
-function updateSquares(){
+function updateSquare(position){
+    let square = document.getElementById(position.toString());
+    let symbol = board[position];
+    square.innerHTML = `<div class='${symbol}'>`
+}
 
+
+function updateSquares(){    
     let squares = document.querySelectorAll(".square");
 
-    squares.forEach((square)=>{
+    squares.forEach((square) => {
         let position = square.id;
         let symbol = board[position];
+
         if(symbol != ''){
-            square.innerHTML = `<div class='${symbol}'>`
+            square.innerHTML = '';
         }
-    });
+    })
 }
